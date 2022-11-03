4cf25bbd97fe6fe875ecee8f78b066f3125a4509
siliziwe <siliziwepani@gmail.com>
2022-11-03 09:37:58 +0200

    updates

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,15 +1,40 @@
 <template>
     <form>
         <label>Email</label>
-        <input type="email" required>
+        <input type="email" required v-model="email">
+        <label>Password</label>
+        <input type="password" required v-model="password">
+
+        <label>Role:</label>
+        <select v-model="role">
+            <option value="developer">Web Developer</option>
+            <option value="designer">Web Designer</option>
+        </select>
+
+        <div class="terms">
+            <input type="checkbox" v-model="terms" required>
+            <label>Ts&Cs</label>
+        </div>
+
     </form>
+    <p>Email: {{ email }}</p>
+    <p>Password: {{ password }}</p>
+    <p>Role: {{ role }}</p>
+    <p>Terms Accepted? {{ terms }}</p>
 </template>
 
 
 
 <script>
 export default {
-
+    data() {
+        return {
+            email: '',
+            password: '',
+            role: 'designer',
+            terms: false
+        }
+    }
 }
 </script>
 
@@ -33,7 +58,7 @@ label {
     letter-spacing: 1px;
     font-weight: bold;
 }
-input {
+input, select {
     display: block;
     padding: 10px 6px;
     width: 100%;
@@ -41,5 +66,12 @@ input {
     border: none;
     border-bottom: 1px solid #ddd;
     color: #555;
+}
+input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
 }
 </style>
